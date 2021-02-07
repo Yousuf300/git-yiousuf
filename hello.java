efa73a7a955c582f78a72811f608c031d861b9ac
md yousuf <freelanceryousuf89@gmail.com>
2021-02-07 22:58:28 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main (string[]args){
         system.out.print("vs code is Awesome...")
+        greeting("md yousuf")
+    }
+    public static void greeting(string name){
+        system.out.println("welcome" + name);
     }
 }
